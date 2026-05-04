6cff043ec6c46bbbe1faf3d31b5c52e9cea32c5f
komed3 <pk97@outlook.de>
2026-05-04 14:13:27 +0200

    setup vite config

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { resolve } from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig ( ( { mode } ) => {
+  return {
+    plugins: [ react(), tailwindcss() ],
+    resolve: { alias: { '@': resolve( __dirname, '.' ) } }
+  };
+} );
