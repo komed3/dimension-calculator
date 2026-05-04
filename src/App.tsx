1bc7f62c32f9de66cacbfb94d29d840410264465
komed3 <pk97@outlook.de>
2026-05-04 14:58:16 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Layout } from './components/Layout';
+import { useCalculator } from './hook/useCalculator';
 
 export default function App () {
+  const calc = useCalculator();
+
   return ( <Layout>
-    <></>
+    <section className="flex flex-col gap-8 lg:col-span-8"></section>
+    <section className="lg:col-span-4 h-full"></section>
   </Layout> );
 };
