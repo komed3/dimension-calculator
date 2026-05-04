9c3cc0b726aa6f3e09f4f64d2bbb58d8297673b8
komed3 <pk97@outlook.de>
2026-05-04 16:54:08 +0200

    split vendor assets

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,16 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig ( ( { mode } ) => {
+export default defineConfig ( () => {
   return {
     plugins: [ react(), tailwindcss() ],
-    resolve: { alias: { '@': resolve( __dirname, '.' ) } }
+    resolve: { alias: { '@': resolve( __dirname, '.' ) } },
+    build: { rollupOptions: { output: { manualChunks ( id ) {
+      if ( id.includes( 'node_modules' ) ) {
+        if ( id.includes( 'lucide' ) ) return 'icons';
+        if ( id.includes( 'react' ) ) return 'react';
+        return 'vendor';
+      }
+    } } } }
   };
 } );
