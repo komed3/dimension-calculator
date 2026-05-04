d64c2107d3fff34327b362c9fb8478ca5a6d1fa4
komed3 <pk97@outlook.de>
2026-05-04 16:35:24 +0200

    create formula from dimension vector

--- src/components/PhysicalForm.tsx
+++ src/components/PhysicalForm.tsx
@@ -1,3 +1,4 @@
+import { Dot } from 'lucide-react';
 import type React from 'react';
 import { DIMENSION_METADATA } from '../const';
 import type { DimensionVector } from '../types';
@@ -7,14 +8,41 @@ interface PhysicalFormProps {
 }
 
 export const PhysicalForm: React.FC< PhysicalFormProps > = ( { vector } ) => {
-  const positive: { symbol: string, value: number }[] = [];
-  const negative: { symbol: string, value: number }[] = [];
+  const positive: React.ReactNode[] = [], negative: React.ReactNode[] = [];
+
+  const renderSymbol = ( s: string, pow: number ) => (
+    <span key={ s } className="inline-flex items-start relative">
+      <span className="leading-none text-[1em] min-w-[0.5em] text-center">{ s }</span>
+      { pow !== 1 && <span className="text-[0.6em] font-serif ml-0.5 mt-[-0.1em]">{ pow }</span> }
+    </span>
+  );
+
+  const joinWithDots = ( elements: React.ReactNode[] ) => {
+    return elements.reduce( ( prev, curr, i ) =>
+      i === 0 ? [ curr ] : [ ...prev, <Dot size={ 28 } key={ `dot-${ i }` } />, curr ],
+      [] as any
+    );
+  };
 
   DIMENSION_METADATA.forEach( ( meta, idx ) => {
     const val = vector[ idx ];
 
-    if ( val === 0 ) return;
-    else if ( val > 0 ) positive.push( { symbol: meta.symbol, value: val } );
-    else negative.push( { symbol: meta.symbol, value: Math.abs( val ) } );
+    if ( val ) ( val > 0 ? positive : negative ).push(
+      renderSymbol( meta.symbol, Math.abs( val ) )
+    );
   } );
+
+  return (
+    <div className="flex flex-col items-center gap-1 font-serif text-3xl">
+      <div className="flex justify-center items-center px-5">
+        { positive.length ? joinWithDots( positive ) : '1' }
+      </div>
+      { negative.length ? ( <>
+        <div className="self-stretch border-t"></div>
+        <div className="flex justify-center items-center px-5">
+          { joinWithDots( negative ) }
+        </div>
+      </> ) : '' }
+    </div>
+  );
 };
