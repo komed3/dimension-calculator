0f1c2bde148f2f7516e8194c0f273a363f3426d0
komed3 <pk97@outlook.de>
2026-05-04 17:11:11 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,15 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig ( () => {
-  return {
-    plugins: [ react(), tailwindcss() ],
-    resolve: { alias: { '@': resolve( __dirname, '.' ) } },
-    build: { rollupOptions: { output: { manualChunks ( id ) {
-      if ( id.includes( 'node_modules' ) ) {
-        if ( id.includes( 'lucide' ) ) return 'icons';
-        if ( id.includes( 'react' ) ) return 'react';
-        return 'vendor';
-      }
-    } } } }
-  };
-} );
+export default defineConfig( () => ( {
+  base: '/dimension-calculator/',
+  plugins: [ react(), tailwindcss() ],
+  resolve: { alias: { '@': resolve( __dirname, '.' ) } },
+  build: { rollupOptions: { output: { manualChunks ( id ) {
+    if ( id.includes( 'node_modules' ) ) {
+      if ( id.includes( 'lucide' ) ) return 'icons';
+      if ( id.includes( 'react' ) ) return 'react';
+      return 'vendor';
+    }
+  } } } }
+} ) );
