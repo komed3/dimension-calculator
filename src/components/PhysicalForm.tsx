85ef2d2646c89807dfd7a886399a3b593c9a9207
komed3 <pk97@outlook.de>
2026-05-04 16:06:40 +0200

    add terms for fraction

--- src/components/PhysicalForm.tsx
+++ src/components/PhysicalForm.tsx
@@ -6,4 +6,15 @@ interface PhysicalFormProps {
   vector: DimensionVector;
 }
 
-export const PhysicalForm: React.FC< PhysicalFormProps > = ( { vector } ) => {};
+export const PhysicalForm: React.FC< PhysicalFormProps > = ( { vector } ) => {
+  const positive: { symbol: string, value: number }[] = [];
+  const negative: { symbol: string, value: number }[] = [];
+
+  DIMENSION_METADATA.forEach( ( meta, idx ) => {
+    const val = vector[ idx ];
+
+    if ( val === 0 ) return;
+    else if ( val > 0 ) positive.push( { symbol: meta.symbol, value: val } );
+    else negative.push( { symbol: meta.symbol, value: Math.abs( val ) } );
+  } );
+};
